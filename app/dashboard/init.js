3a0d5cbdd956dd681389280e4b259d09a0932e2e
NihalSh <nihal.sh.1995@gmail.com>
2017-03-15 11:00:40 +0530

    dashboard updated

--- app/dashboard/init.js
+++ app/dashboard/init.js
@@ -2,6 +2,12 @@ const passport = require('passport')
 
 module.exports = (app) => {
 	app.get('/dashboard', passport.authenticationMiddleware(), (req, res) => {
-		res.render('dashboard/dashboard')
+		res.render('dashboard/dashboard', {
+			name: req.user.name,
+			registrationNumber: req.user.registrationNumber,
+			program: req.user.program,
+			department: req.user.department,
+			semester: req.user.semester,
+		})
 	})
 }
