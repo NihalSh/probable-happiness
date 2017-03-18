c620de51db4761bedf4a169a2cb230d51ac79579
NihalSh <nihal.sh.1995@gmail.com>
2017-03-18 16:25:48 +0530

    skills and interests POST request handler

--- app/dashboard-skillsAndInterests/init.js
+++ app/dashboard-skillsAndInterests/init.js
@@ -1,18 +1,55 @@
 const passport = require('passport')
 
+const User = require('../user').model
+
 module.exports = (app) => {
 	app.get('/dashboard/skillsAndInterests', passport.authenticationMiddleware(), (req, res) => {
+		let programmingLanguages = null
+		let languages = null
+		let cocurricular = null
+		let extracurricular = null
+		if (req.user.programmingLanguages) {
+			programmingLanguages = req.user.programmingLanguages.join(', ')
+		}
+		if (req.user.languages) {
+			languages = req.user.languages.join(', ')
+		}
+		if (req.user.cocurricular) {
+			cocurricular = req.user.cocurricular.join(', ')
+		}
+		if (req.user.extracurricular) {
+			extracurricular = req.user.extracurricular.join(', ')
+		}
 		res.render('dashboard-skillsAndInterests/dashboard-skillsAndInterests',
 			{
 				name: req.user.name,
-				programmingLanguages: req.user.programmingLanguages,
-				languages: req.user.languages,
-				cocurricular: req.user.cocurricular,
-				extracurricular: req.user.extracurricular
+				programmingLanguages: programmingLanguages,
+				languages: languages,
+				cocurricular: cocurricular,
+				extracurricular: extracurricular
 			}
 		)
 	})
 	app.post('/dashboard/skillsAndInterests', passport.authenticationMiddleware(), (req, res) => {
-		res.send(req.body)
+		let programmingLanguages = req.body.programmingLanguages.split(',').map(Function.prototype.call, String.prototype.trim)
+		let languages = req.body.languages.split(',').map(Function.prototype.call, String.prototype.trim)
+		let cocurricular = req.body.cocurricular.split(',').map(Function.prototype.call, String.prototype.trim)
+		let extracurricular = req.body.extracurricular.split(',').map(Function.prototype.call, String.prototype.trim)
+		User.findOneAndUpdate({ email: req.user.email },
+			{
+				$set: {
+					'programmingLanguages': programmingLanguages,
+					'languages': languages,
+					'cocurricular': cocurricular,
+					'extracurricular': extracurricular
+				}
+			})
+			.exec()
+			.then(() => {
+				res.redirect('/dashboard/skillsAndInterests')
+			})
+			.catch((err) => {
+				next(err)
+			})
 	})
 }
