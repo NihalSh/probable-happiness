cc1cb1c2ccdf0fe4c708406ae1174f7aa32dae2d
NihalSh <nihal.sh.1995@gmail.com>
2017-03-18 15:48:01 +0530

    academics GET updated

--- app/dashboard-academics/init.js
+++ app/dashboard-academics/init.js
@@ -11,14 +11,14 @@ module.exports = (app) => {
 				'10board': req.user["10board"],
 				'12pc': req.user["12pc"],
 				'12board': req.user["12board"],
-				sgpa1: 5,
-				sgpa2: 6,
-				sgpa3: 7,
-				sgpa4: 8,
-				sgpa5: 9,
-				cgpa: 10,
-				historyArrears: false,
-				standingArrears: 3
+				sgpa1: req.user.sgpa1,
+				sgpa2: req.user.sgpa2,
+				sgpa3: req.user.sgpa3,
+				sgpa4: req.user.sgpa4,
+				sgpa5: req.user.sgpa5,
+				cgpa: req.user.cgpa,
+				historyArrears: null,
+				standingArrears: null
 			}
 		)
 	})
