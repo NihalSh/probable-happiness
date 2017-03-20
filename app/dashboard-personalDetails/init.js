d720281def0380c7db04a5543359b09eaaae8ad2
NihalSh <nihal.sh.1995@gmail.com>
2017-03-20 10:36:42 +0530

    personal details - handler updated

--- app/dashboard-personalDetails/init.js
+++ app/dashboard-personalDetails/init.js
@@ -3,28 +3,7 @@ const passport = require('passport')
 const User = require('../user').model
 
 module.exports = (app) => {
-	app.get('/dashboard/personalDetails', passport.authenticationMiddleware(), (req, res) => {
-		let date = null
-		if (req.user.dateOfBirth) {
-			date = req.user.dateOfBirth.toDateString()
-		}
-		res.render('dashboard-personalDetails/dashboard-personalDetails',
-			{
-				name: req.user.name,
-				regno: req.user.registrationNumber,
-				boarding: req.user.boarding,
-				bloodGroup: req.user.bloodGroup,
-				dob: date,
-				gender: req.user.gender,
-				height: req.user.height,
-				weight: req.user.weight,
-				fName: req.user.fatherName,
-				mName: req.user.motherName,
-				evarsity: req.user.evarsity,
-				image: req.user.image
-			}
-		)
-	})
+	app.get('/dashboard/personalDetails', passport.authenticationMiddleware(), getHandler)
 	app.post('/dashboard/personalDetails', passport.authenticationMiddleware(), (req, res) => {
 		let boarding = null
 		if (req.body.boarding === "true") {
@@ -40,28 +19,45 @@ module.exports = (app) => {
 					'weight': req.body['weight'],
 					'motherName': req.body['mName']
 				}
-			})
+			},
+			{
+				new: true
+			}
+			)
 			.exec()
-			.then(() => {
-				res.render('dashboard-personalDetails/dashboard-personalDetails',
-					{
-						script: `
-							notie.alert({ type: 'success', text: 'Response recorded', time: 3});
-							setTimeout( function() { window.location.href = '/dashboard/personalDetails';}, 3100);
-
-							`
-					})
+			.then((doc) => {
+				req.user = doc
+				req.script = `notie.alert({ type: 'success', text: 'Response recorded', time: 3});`
+				getHandler(req, res)
 			})
 			.catch((err) => {
 				req.log.error(err)
-				res.render('dashboard-personalDetails/dashboard-personalDetails',
-					{
-						script: `
-							notie.alert({ type: 'error', text: 'Check the data and try again', time: 3});
-							setTimeout( function() { window.location.href = '/dashboard/personalDetails';}, 3100);
-
-							`
-					})
+				req.script = `notie.alert({ type: 'error', text: 'Check the data and try again', time: 3});`
+				getHandler(req, res)
 			})
 	})
+}
+
+function getHandler(req, res) {
+	let date = null
+	if (req.user.dateOfBirth) {
+		date = req.user.dateOfBirth.toDateString()
+	}
+	res.render('dashboard-personalDetails/dashboard-personalDetails',
+		{
+			name: req.user.name,
+			regno: req.user.registrationNumber,
+			boarding: req.user.boarding,
+			bloodGroup: req.user.bloodGroup,
+			dob: date,
+			gender: req.user.gender,
+			height: req.user.height,
+			weight: req.user.weight,
+			fName: req.user.fatherName,
+			mName: req.user.motherName,
+			evarsity: req.user.evarsity,
+			image: req.user.image,
+			script: req.script
+		}
+	)
 }
