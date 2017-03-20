fac1ab6fe6280785e17ad09f5079f999ccc20e62
NihalSh <nihal.sh.1995@gmail.com>
2017-03-20 10:25:47 +0530

    dashboard more modular

---
+++ app/dashboard/handler.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+	res.render('dashboard/dashboard',
+		{
+			name: req.user.name,
+			registrationNumber: req.user.registrationNumber,
+			program: req.user.program,
+			department: req.user.department,
+			semester: req.user.semester,
+			evarsity: req.user.evarsity,
+			image: req.user.image
+		}
+	)
+}
--- app/dashboard/init.js
+++ app/dashboard/init.js
@@ -1,17 +1,7 @@
 const passport = require('passport')
 
+const handler = require('./handler')
+
 module.exports = (app) => {
-	app.get('/dashboard', passport.authenticationMiddleware(), (req, res) => {
-		res.render('dashboard/dashboard',
-			{
-				name: req.user.name,
-				registrationNumber: req.user.registrationNumber,
-				program: req.user.program,
-				department: req.user.department,
-				semester: req.user.semester,
-				evarsity: req.user.evarsity,
-				image: req.user.image
-			}
-		)
-	})
+	app.get('/dashboard', passport.authenticationMiddleware(), handler)
 }
